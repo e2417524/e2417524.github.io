6d122492a986da6d3f54d49fb6a352b78b2988bf
e2417524 <e2417524@soka-u.jp>
2025-07-31 05:38:41 +0900

    最終課題更新

--- js/jquery-final.js
+++ js/jquery-final.js
@@ -1,12 +1,7 @@
 console.log("jQuery-final.jsを読み込みます")
 
 
-$(function () {
-  $('.member-details').hide();　//メンバーインスタをはじめは隠す
-});
-$("h3").on("click",function(){ //メンバーの名前をクリックした時
-    $(this).next().slideToggle();　//スライドトグルする
-});
+
 
 
 //1
@@ -23,43 +18,21 @@ $(function () {
   );
 });
 
-//2
-$(function () {
-  $('.miselle').hide();
 
-  $('.menumem').hover(
+$(function () {
+  $('.mvint').hide();
+
+  $('.menumv').hover(
     function () {
-      $(this).find('.miselle').stop(true, true).slideDown('slow');
+      $(this).find('.mvint').stop(true, true).slideDown('slow');
     },
     function () {
-      $(this).find('.miselle').stop(true, true).slideUp('slow');
+      $(this).find('.mvint').stop(true, true).slideUp('slow');
     }
   );
 });
 
-$(function () {
-  $('.minter').hide();
 
-  $('.menumem').hover(
-    function () {
-      $(this).find('.minter').stop(true, true).slideDown('slow');
-    },
-    function () {
-      $(this).find('.minter').stop(true, true).slideUp('slow');
-    }
-  );
-});
 
-$(function () {
-  $('.mingning').hide();
 
-  $('.menumem').hover(
-    function () {
-      $(this).find('.mingning').stop(true, true).slideDown('slow');
-    },
-    function () {
-      $(this).find('.mingning').stop(true, true).slideUp('slow');
-    }
-  );
-});
 
